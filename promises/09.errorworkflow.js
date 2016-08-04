ea532980b3cd5eff38cdf6934c0cadbe8f793c4a
zb <ztenerowicz@egnyte.com>
2016-08-04 09:35:48 +0200

    add promises concurrency and error handling workflow tasks

---
+++ promises/09.errorworkflow.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const fakeDBContains = require('db')
+
+const db = fakeDBContains(['list','item1','item2','item3','item1details','item2details','item3details'])
+//variants of broken data:
+// const db = fakeDBContains(['list','item1','item2','item3'])
+// const db = fakeDBContains(['list','item1','item2','item3','item1details','item2details'])
+// const db = fakeDBContains(['list','item1','item2'])
+// const db = fakeDBContains(['list','item1','item2','item1details','item2details','item3details'])
+
+//TODO: get list of items, based on that get 3 items, based on that get details for every item.
+
+db.get('list')
+db.get('item1')
+db.get('item2')
+db.get('item3')
+db.get('item1details')
+db.get('item2details')
+db.get('item3details')
