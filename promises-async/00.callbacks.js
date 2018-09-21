41edd14cf83b614aa5cb7bd14e8668c4e0c147d8
Naugtur <naugtur@gmail.com>
2018-09-22 00:00:55 +0200

    add callback demo

---
+++ promises-async/00.callbacks.js
@@ -0,0 +1,35 @@
+
+function a() {
+    console.log(2)
+}
+console.log(1)
+a()
+console.log(3)
+
+
+setImmediate(() => {
+    console.log('5.c');
+})
+setTimeout(() => {
+    console.log('5.b');
+}, 0)
+process.nextTick(() => {
+    console.log('5.a');
+})
+console.log(4);
+
+
+// function noop(){}
+
+// async function b(){
+//     const x = await noop()
+//     console.log('6.b')
+// }
+// b()
+// console.log('6.a');
+
+// Promise.resolve().then(() => {
+//     console.log('7.b');
+// })
+// console.log('7.a');
+
