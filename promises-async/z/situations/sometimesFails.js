33a343f9f3312c37e8fdb968258536318d2e3086
naugtur <naugtur@gmail.com>
2020-07-06 15:27:59 +0200

    fix path

--- promises-async/z/situations/sometimesFails.js
+++ promises-async/z/situations/sometimesFails.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = function sometimesFails() {
-    return require('helpers').delay().then(() => {
+    return require('../helpers').delay().then(() => {
         if (Math.random() > 0.5) {
             console.log('>> fail')
             throw Error('failure')
